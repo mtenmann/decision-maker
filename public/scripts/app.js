b6276a19aae97deeab33d3f717490cb0ba64cc74
Magnus <massedritmail@gmail.com>
2017-12-03 17:47:47 +0100

    Added package info.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,23 @@
+"use strict";
+
 console.log('App.js is running!');
 
 // JSX - Is extension for JavaScript XML, given to us by React.
 
-// var template  = <p>This is from the JSX in the app.js</p>;
-
 var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
     "h1",
-    { id: "app" },
-    "Something new"
-  );
+    null,
+    "Decision Helper"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  )
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
